0fe82d043e222b855a65963abdc943f01675c207
Ben Hutchison <ben@aldaviva.com>
2020-06-22 00:45:58 -0700

    Leave VMware Authorization Service running in Windows 10, since VMware Workstation can't start guest VMs if this service is stopped.

--- KillUnwantedProcesses/KillableProcesses/VmAuthdService.cs
+++ KillUnwantedProcesses/KillableProcesses/VmAuthdService.cs
@@ -1,4 +1,5 @@
-﻿using System.ServiceProcess;
+﻿using System;
+using System.ServiceProcess;
 
 namespace KillUnwantedProcesses.KillableProcesses {
 
@@ -8,8 +9,12 @@ namespace KillUnwantedProcesses.KillableProcesses {
 
         public override string name { get; } = "VMware Authorization Service";
 
+        /// <summary>
+        /// On Windows 7, a VMware Workstation can start a guest when this service is disabled. On Windows 10, it cannot, and fails with an error message telling the user to enable this service.
+        /// </summary>
+        /// <returns></returns>
         public override bool shouldKill() {
-            return isServiceRunning(SERVICE_NAME) && !isProcessRunning("vmware-vmx");
+            return Environment.OSVersion.Version.Major <= 7 && isServiceRunning(SERVICE_NAME) && !isProcessRunning("vmware-vmx");
         }
 
         public override void kill() {
